5aaf1a0b5740537d38293d03a6311c5af49a9ddc
MikeW97 <mikewilkins97@gmail.com>
2021-04-08 15:50:37 +0200

    Added functionality to read matrices in from a file

--- main.cpp
+++ main.cpp
@@ -1,114 +1,81 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include <ctime>
-#include <stdexcept>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iterator>
 
-using std::cin;
-using std::cout;
-using std::invalid_argument;
-using namespace std; 
-
-int main()
-{   
-    //Random Number generator 
-    srand((unsigned)time(0));
-
-    int bounds;
-    int dimensions;
-    int randNo;
-
-    cout << "Please enter the bounds of the arrays: " << '\n';
-    cin >> bounds;
-
-    if ((bounds % 10 != 0) || bounds > 30)
-        throw invalid_argument("Input must be 10, 20 or 30!");
-
-    cout << "Please enter the dimensions of the arrays: " << '\n';
-    cin >> dimensions;
-
-    if (dimensions != 2 && dimensions != 3)
-        throw invalid_argument("Dimensions must be either 2 or 3!");
-
-    for (int dim = 0; dim < dimensions; dim++)
-    {
-        for (int col = 0; col < bounds; col++)
-        {
-            for (int row = 0; row < bounds; row++)
-            {
-                randNo = (rand() % 21);
-                cout << randNo << ' ';
-            }
-
-            cout << '\n';
-        }
-=======
 #include "matrixGen.h"
+
+using namespace std;
 
 int main()
 {
-    int bounds = 30;
+    int bounds = 10;
     int dimensions = 2;
->>>>>>> randNoRefactor
 
     matrixGen(bounds, dimensions);
 
-    // 2D Multiplication 
-        vector<vector<int>> matrixA
+    ifstream firstMat("matrixA.txt");
+    vector<vector<int>> matrixA;
+    string line;
+    while (getline(firstMat, line))
     {
-        {1, 2, 3, 5, 4},
-        {5, 1, 3, 2, 6},
-        {2, 4, 1, 6, 2},
-        {5, 1, 3, 2, 6},
-        {2, 4, 1, 6, 2},
-    };
-        vector<vector<int>> matrixB
+        istringstream is(line);
+        matrixA.push_back(
+            vector<int>(istream_iterator<int>(is),
+                        istream_iterator<int>()));
+    }
+
+    firstMat.close();
+
+    ifstream secondMat("matrixB.txt");
+    vector<vector<int>> matrixB;
+    while (getline(secondMat, line))
     {
-        {1, 4 ,1, 4, 6},
-        {2, 3 ,2, 2, 3},
-        {1, 2 ,3, 5, 4},
-        {2, 3 ,2, 3, 1},
-        {1, 2 ,3, 2, 4},
-    };
+        istringstream is(line);
+        matrixB.push_back(
+            vector<int>(istream_iterator<int>(is),
+                        istream_iterator<int>()));
+    }
 
     //First Check if columns of A = rows of B
     int colA = matrixA[0].size();
     int rowB = matrixB.size();
 
-    //set the resultant vector dimensions and do the multiplication 
+    //set the resultant vector dimensions and do the multiplication
     int rowC = matrixA.size();
-    int colC = matrixB[0].size() ;
-    vector<vector<int>> matrixC ( rowC , vector<int> (colC));
+    int colC = matrixB[0].size();
+    vector<vector<int>> matrixC(rowC, vector<int>(colC));
 
-    if (colA==rowB) 
-    {          
-        int i, j, k;
-        int N = colA; 
-    
-        for (i = 0; i < N; i++) 
-            {
-                for (j = 0; j < N; j++) 
-                    {
-                        matrixC[i][j] = 0;
-                        
-                        for (k = 0; k < N; k++)
-                        {
-                            matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
-                        }
-                            
-                    }
-            }
-
-    }
-    else 
+    if (colA == rowB)
     {
-        //can implement some sort of error once function is set using throw and catch. 
+        int i, j, k;
+        int N = colA;
+
+        for (i = 0; i < N; i++)
+        {
+            for (j = 0; j < N; j++)
+            {
+                matrixC[i][j] = 0;
+
+                for (k = 0; k < N; k++)
+                {
+                    matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+    }
+    else
+    {
+        //can implement some sort of error once function is set using throw and catch.
     }
 
-   // Just checking the results of the multiplication. 
-   cout << "Result matrix is \n";
-    for (int i = 0; i < rowC; i++) {
+    // Just checking the results of the multiplication.
+    cout << "Result matrix is \n";
+    for (int i = 0; i < rowC; i++)
+    {
         for (int j = 0; j < colC; j++)
             cout << matrixC[i][j] << " ";
         cout << "\n";
--- matrixGen.cpp
+++ matrixGen.cpp
@@ -9,38 +9,76 @@ using std::cout;
 void matrixGen(int bounds, int dimensions)
 {
     srand((unsigned)time(0));
-
-    std::ofstream matrices("matrices.txt");
-
     int randNo;
 
-    for (int dim = 0; dim < dimensions; dim++)
+    std::ofstream matrixA("matrixA.txt");
+
+    for (int col = 0; col < bounds; col++)
     {
+        for (int row = 0; row < bounds; row++)
+        {
+            randNo = (rand() % 21);
+
+            if (matrixA.is_open())
+                matrixA << randNo << ' ';
+            else
+                cout << "Unable to open file \n";
+        }
+
+        if (matrixA.is_open())
+            matrixA << '\n';
+        else
+            cout << "Unable to open file \n";
+    }
+
+    matrixA.close();
+    std::ofstream matrixB("matrixB.txt");
+
+    for (int col = 0; col < bounds; col++)
+    {
+        for (int row = 0; row < bounds; row++)
+        {
+            randNo = (rand() % 21);
+
+            if (matrixB.is_open())
+                matrixB << randNo << ' ';
+            else
+                cout << "Unable to open file \n";
+        }
+
+        if (matrixB.is_open())
+            matrixB << '\n';
+        else
+            cout << "Unable to open file \n";
+    }
+
+    matrixB.close();
+    if (dimensions == 3)
+    {
+        std::ofstream matrixC("matrixC.txt");
+
         for (int col = 0; col < bounds; col++)
         {
             for (int row = 0; row < bounds; row++)
             {
                 randNo = (rand() % 21);
 
-                if (matrices.is_open())
-                    matrices << randNo << ' ';
+                if (matrixC.is_open())
+                    matrixC << randNo << ' ';
                 else
                     cout << "Unable to open file \n";
             }
 
-            if (matrices.is_open())
-                matrices << '\n';
+            if (matrixC.is_open())
+                matrixC << '\n';
             else
                 cout << "Unable to open file \n";
         }
 
-        if (matrices.is_open())
-            matrices << '\n';
-        else
-            cout << "Unable to open file \n";
+        matrixC.close();
     }
-
-    matrices.close();
+    else
+        return;
 
     return;
 }
